bda50488a763bb6699bbd7947c99d01aa48effa7
Krishna-dev7 <suprabha205@gmail.com>
2024-12-29 16:04:54 +0530

    change conf

--- src/helpers/conf.ts
+++ src/helpers/conf.ts
@@ -3,7 +3,7 @@ const conf = {
     resend: process.env.RESEND_API_KEY,
     githubClientID: process.env.GITHUB_CLIENT_ID,
     githubSecret: process.env.GITHUB_CLIENT_SECRET,
-    url: process.env.NEXT_PUBLIC_APP_URL
+    url: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000"
 }
 
 export default conf;
