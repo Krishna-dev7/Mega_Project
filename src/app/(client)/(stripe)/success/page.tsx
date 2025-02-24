8b417fe0022069fab32267bc83efe351c4514210
Krishna-dev7 <suprabha205@gmail.com>
2025-02-24 14:42:39 +0530

    packages updated

--- src/app/(client)/(stripe)/success/page.tsx
+++ src/app/(client)/(stripe)/success/page.tsx
@@ -14,11 +14,16 @@ const SuccessPage: React.FC = () => {
   })
 
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-100">
-      <div className="bg-white rounded-lg shadow-lg p-8 text-center">
-        <h1 className="text-2xl font-bold text-green-600">Payment Successful!</h1>
+    <div className="flex h-screen items-center
+       justify-center bg-gray-100">
+      <div className="bg-white rounded-lg
+         shadow-lg p-8 text-center">
+        <h1 className="text-2xl font-bold text-green-600">
+          Payment Successful!
+          </h1>
         <p className="mt-4 text-gray-600">
-          Thank you for your payment. Your transaction was successful.
+          Thank you for your payment. 
+          Your transaction was successful.
         </p>
         <button
           onClick={handleReturnHome}
